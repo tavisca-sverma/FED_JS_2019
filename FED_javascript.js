38c1ddd71483953e65641951ee1c27d0d7b9cbbc
tavisca-sverma <51913758+tavisca-sverma@users.noreply.github.com>
2019-08-21 01:26:01 +0530

    Added Todo  with Autocomplete feature in search

---
+++ FED_javascript.js
@@ -0,0 +1,55 @@
+// JavaScript source code
+function showSuggestions() {
+    document.getElementsByClassName("autocomplete")[0].style.display = 'block';
+}
+
+function hideSuggestions() {
+    document.getElementsByClassName("autocomplete")[0].style.display = 'none';
+}
+
+function showTab(tabname) {
+    let x = document.querySelectorAll(".content-tab");
+    let len = x[0].childElementCount;//get number of childs(divisions) in 1st class element
+
+    for (let i = 0; i < len; i++) {
+        x[0].children[i].style.display = 'none';
+    }
+
+    document.getElementById("tab-" + tabname).style.display = 'block';
+}
+
+var todoList = ["brush", "run", "bath", "play", "code", "sleep", "eat","clean"];
+
+
+function createAutoCompleteList() {
+
+    clearAutoCompleteList();
+
+    let searchKey = document.getElementsByName("searchbox")[0].value;//get value in textbox
+
+    if (searchKey == "") {
+        return;
+    }
+      
+    for (let i = 0; i < todoList.length; i++) {
+        if (todoList[i].startsWith(searchKey)) {
+
+            var listItem = document.createElement("li");
+            var node = document.createTextNode(todoList[i]);
+            listItem.appendChild(node);
+
+            var element = document.getElementById("autocomplete-list");
+            element.appendChild(listItem);
+        }
+    }
+}
+
+    function clearAutoCompleteList() {
+        var element = document.getElementById("autocomplete-list");
+        while (element.lastElementChild != null) {
+            element.removeChild(element.lastElementChild);
+        }
+        /*for (let i = 0; i < element.childElementCount; i++) {
+          
+        }*/
+   }
